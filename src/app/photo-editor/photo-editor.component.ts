34374ba24b300169d631e77642be496565c5a885
arta-eth <arta@verticalai.io>
2023-06-06 02:21:30 -0400

    o

--- src/app/photo-editor/photo-editor.component.ts
+++ src/app/photo-editor/photo-editor.component.ts
@@ -5,23 +5,29 @@ import {
   Input,
   ElementRef,
   OnInit,
+  HostListener,
+  NgZone,
+  ChangeDetectorRef,
 } from '@angular/core';
-import { PhotoEditorSDKUI, EditorApi, ContainedPrimaryButton } from 'photoeditorsdk/no-polyfills';
+import {
+  PhotoEditorSDKUI,
+  EditorApi,
+  ContainedPrimaryButton,
+} from 'demo-vai/no-polyfills';
 import styled from 'styled-components';
 
-
 const license = '';
-
 
 @Component({
   selector: 'app-photo-editor',
   templateUrl: './photo-editor.component.html',
 })
 export class PhotoEditorComponent implements OnInit, AfterViewInit {
-
   ngOnInit(): void {
-
+    this.loading = true;
   }
+
+  constructor(private zone: NgZone, private cdr: ChangeDetectorRef) {}
 
   @Input()
   public src: string = '';
@@ -33,15 +39,19 @@ export class PhotoEditorComponent implements OnInit, AfterViewInit {
 
   @ViewChild('psdkContainer', { static: false })
   private container: ElementRef<HTMLDivElement> | null = null;
-
+  loading = false;
 
   public editor: EditorApi | null = null;
-
 
   ngAfterViewInit() {
     this.initEditor();
   }
 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    // console.log(event);
+    // this.redrawCanvas();
+  }
 
   async initEditor() {
     try {
@@ -56,13 +66,22 @@ export class PhotoEditorComponent implements OnInit, AfterViewInit {
         assetBaseUrl: '/assets/photoeditorsdk',
       });
 
-      let btn = document.querySelector('[data-test="MainBarButtonClose"]')
+      let btn = document.querySelector('[data-test="MainBarButtonClose"]');
+      // let btn2 = document.querySelector('[data-test="MainBarButtonExport"]');
 
-      btn.remove()
-      console.log(btn)
+      btn.remove();
+      // btn2.remove();
+      // let canvas = document.querySelector('[data-test="Canvas"]');
+      let w = document.querySelector('[data-test="Watermark"]');
+
+      // canvas.remove();
+      w.remove();
+
       // this.editor.on('close' as any, () => this.editor.dispose());
     } catch (error) {
       console.log(error);
     }
   }
+
+
 }
